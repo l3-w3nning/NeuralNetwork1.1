f940355cf371de4c239c587d23a1c4bf49e3dc08
l3-w3nning <115408391+l3-w3nning@users.noreply.github.com>
2022-11-25 21:11:38 +0100

    Add files via upload

--- StepByStepNN.R
+++ StepByStepNN.R
@@ -1,8 +1,8 @@
 
-data <- data.frame(x=runif(1000),y=runif(1000)) %>% 
-        mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) 
-
-data %>% ggplot(aes(x=x,y=y,colour=label))+geom_point()+theme_bw()
+# data <- data.frame(x=runif(1000),y=runif(1000)) %>% 
+#         mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) 
+# 
+# data %>% ggplot(aes(x=x,y=y,colour=label))+geom_point()+theme_bw()
 
 classifier <- function(point_x,point_y,weight_matrix,bias){
   point_xy=c(point_x,point_y)
@@ -14,9 +14,68 @@ get_weight_matrix_and_bias <- function(w_11,w_12,w_21,w_22,b_1,b_2){
   return(c(mat=matrix(c(w_11,w_12,w_21,w_22),nrow=2,ncol=2),bias=c(b_1,b_2)))
 }
 
+
+
 decision_boundary <- function(weight_matrix,bias){
   return(c(m=(weight_matrix[2,2]-weight_matrix[1,2])/(weight_matrix[1,1]-weight_matrix[1,2]),b=bias[2]-bias[1]))
 }
 
+library(methods)
 
+layer <- setRefClass("Layer", fields = list(num_nodes_in = "numeric", 
+                                              num_nodes_out = "numeric",weights = "matrix", bias = "numeric"), methods = list(
+                                                initialize = function(num_nodes_in,num_nodes_out,weights,bias){
+                                                  .self$num_nodes_in <- num_nodes_in
+                                                  .self$num_nodes_out <- num_nodes_out
+                                                  mat=matrix(weights,nrow=num_nodes_in,ncol=num_nodes_out)
+                                                  .self$weights = mat
+                                                  .self$bias = bias
+                                                },
+                                                CalcOutput = function(input)
+                                                {
+                                                  
+                                                  output =.self$bias
+                                                  output = output + input %*% .self$weights
+                                                  
+                                                  print(output)
+                                                  return (output)
+                                                }
+                                              ))
+
+
+NeuralNetwork <- setRefClass("NN",fields = list(layer_sizes = "numeric",num_nodes_out="numeric",weights = "list",bias = "list"),
+                                            #initialize nn by creating list of layers (with given layer sizes)
+                                            #then write a method which runs inputs through layers (CalcOutput)
+                                            #weights ist hier eine list of lists
+                                            #biases ist hier auch eine list of lists
+                                            methods = list(
+                                                  intialize = function(layer_sizes,weights,bias){
+                                                   .self$layer_sizes <- layer_sizes
+                  
+                                                  },
+                                                  
+                                                  GetLayers = function(){ 
+                                                    layers=list()
+                                                    for (i in 1:(length(.self$layer_sizes)-1)){
+                                                      layers <- append(layers,layer$new(num_nodes_in=.self$layer_sizes[i],num_nodes_out=.self$layer_sizes[i+1],weights=.self$weights[[i]],bias=.self$bias[[i]]))
+                                                    }
+                                                    return(layers)
+                                                  },
+                                                  
+                                                  CalcOutputNN = function(inputx,inputy)
+                                                  {
+                                                    input = c(inputx,inputy)
+                                                    layers = .self$GetLayers()
+              
+                                                    for (layer in layers){
+                                                      input = layer$CalcOutput(input)
+                                                      
+                                                    }
+                                                    output=input
+                                                    return (output)
+                                                  },
+                                                  Classify = function(output){
+                                                    return(ifelse(output[1]>=output[2],1,0))
+                                                  }
+                                                ))
 
--- app.R
+++ app.R
@@ -17,8 +17,6 @@ source(paste0(path,"/StepByStepNN.R"))
 consideredDigits <- 3
 stepWidth <- 1/10^(consideredDigits+1)
 
-data <- data.frame(x=runif(3600),y=runif(3600)) %>% 
-  mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -27,14 +25,25 @@ ui <- fluidPage(
     titlePanel("Simple DecisionBoundary Tool"),
 
     # Sidebar with a slider input for number of bins 
-    headerPanel("Linear Decision Boundary Params"),
+    headerPanel("Decision Boundary Params (One Hidden Layer with 3 Nodes)"),
     sidebarPanel(
       sliderInput(inputId = "W11", label = "W11", min = -1, max = 1, value = 0.2, step = stepWidth),
       sliderInput(inputId = "W12", label = "W12", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "W21", label = "W21", min = -1, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "W13", label = "W13", min = -1, max = 1, value = 0.2, step = stepWidth),
+      sliderInput(inputId = "W21", label = "W21", min = -1, max = 1, value = 0.2, step = stepWidth),
       sliderInput(inputId = "W22", label = "W22", min = -1, max = 1, value = 0.6, step = stepWidth),
-      sliderInput(inputId = "bias1", label = "b1", min = 0, max = 1, value = 0.6, step = stepWidth),
-      sliderInput(inputId = "bias2", label = "b2", min = 0, max = 1, value = 0.6, step = stepWidth)
+      sliderInput(inputId = "W23", label = "W23", min = -1, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "bias1_1", label = "b1_1", min = 0, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "bias2_1", label = "b2_1", min = 0, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "bias3_1", label = "b3_1", min = 0, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "A11", label = "A11", min = -1, max = 1, value = 0.2, step = stepWidth),
+      sliderInput(inputId = "A21", label = "A21", min = -1, max = 1, value = 0.2, step = stepWidth),
+      sliderInput(inputId = "A31", label = "A31", min = -1, max = 1, value = 0.2, step = stepWidth),
+      sliderInput(inputId = "A12", label = "A12", min = -1, max = 1, value = 0.2, step = stepWidth),
+      sliderInput(inputId = "A22", label = "A22", min = -1, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "A32", label = "A32", min = -1, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "bias1_2", label = "b1_2", min = -1, max = 1, value = 1, step = stepWidth),
+      sliderInput(inputId = "bias2_2", label = "b2_2", min = -1, max = 1, value = 0, step = stepWidth)
     ),
     mainPanel(),
 
@@ -49,29 +58,35 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
       
-    reactive_fun <- reactive({get_weight_matrix_and_bias(as.numeric(input$W11),as.numeric(input$W12),as.numeric(input$W21),as.numeric(input$W22),as.numeric(input$b1),as.numeric(input$b2))})
-      
+    
+    data <- data.frame(x=runif(100),y=runif(100)) %>% 
+      mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) 
       
     output$ClassifierPlot <- renderPlot({
         
         # generate bins based on input$bins from ui.R
-        mat = matrix(c(input$W11,input$W12,input$W21,input$W22),nrow=2,ncol=2)
-        bias = c(input$bias1,input$bias2)
-        print(bias)
-        print(mat)
-        m <- (mat[2,2]-mat[1,2])/(mat[1,1]-mat[2,1])
-        b <- bias[2]-bias[1]
-        eq = function(x){m*x+b}
+        mat_1 = matrix(c(input$W11,input$W12,input$W13,input$W21,input$W22,input$W23),nrow=2,ncol=3)
+        mat_2 = matrix(c(input$A11,input$A21,input$A31,input$A12,input$A22,input$A32),nrow=3,ncol=2)
+        weights_list = list(mat_1,mat_2)
+        bias_1 = c(input$bias1_1,input$bias2_1)
+        bias_2 = c(input$bias1_2,input$bias2_2)
+        biases = list(bias_1,bias_2)
+        layer_sizes = c(2,3,2)
         
-        x=seq(from=0,to=1,length.out=60)
+        nn <- NeuralNetwork$new(layer_sizes=layer_sizes,num_nodes_out=2,weights=weights_list,bias=biases)
+        
+        x=seq(from=0,to=1,length.out=10)
         y=x
         data_grid=expand.grid(x=x,y=y)
-        z=mapply(classifier,data_grid$x,data_grid$y,MoreArgs = list(mat,bias))
-    
+        
+        output=mapply(nn$CalcOutputNN,data_grid$x,data_grid$y) #to visualize decision boundary
+  
+        z=apply(output,MARGIN=2,nn$Classify)
+  
         data_grid <- data_grid %>% mutate(z=z)
         
         v <- ggplot(data_grid, aes(x, y, fill= z)) + 
-          geom_tile(alpha=0.4)
+          geom_tile(alpha=0.2)
         v<-v + geom_point(data=data, aes(x=x, y=y,colour=label))
         print(v)
         

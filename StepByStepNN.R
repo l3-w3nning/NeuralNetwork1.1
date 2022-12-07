d0c68119f8dafcd18fa656a0b657cfac50c24c77
l3-w3nning <115408391+l3-w3nning@users.noreply.github.com>
2022-12-07 05:56:45 +0100

    Add files via upload

--- StepByStepNN.R
+++ StepByStepNN.R
@@ -1,35 +1,26 @@
 
-# data <- data.frame(x=runif(1000),y=runif(1000)) %>% 
-#         mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) 
-# 
-# data %>% ggplot(aes(x=x,y=y,colour=label))+geom_point()+theme_bw()
-
-classifier <- function(point_x,point_y,weight_matrix,bias){
-  point_xy=c(point_x,point_y)
-  res <- weight_matrix%*%point_xy+bias
-  return (ifelse(res[1]>=res[2],1,0)) #binary classifier, later used for contour
-}
-
-get_weight_matrix_and_bias <- function(w_11,w_12,w_21,w_22,b_1,b_2){
-  return(c(mat=matrix(c(w_11,w_12,w_21,w_22),nrow=2,ncol=2),bias=c(b_1,b_2)))
-}
-
-
-
-decision_boundary <- function(weight_matrix,bias){
-  return(c(m=(weight_matrix[2,2]-weight_matrix[1,2])/(weight_matrix[1,1]-weight_matrix[1,2]),b=bias[2]-bias[1]))
-}
 
 library(methods)
 
 layer <- setRefClass("Layer", fields = list(num_nodes_in = "numeric", 
-                                              num_nodes_out = "numeric",weights = "matrix", bias = "numeric"), methods = list(
-                                                initialize = function(num_nodes_in,num_nodes_out,weights,bias){
+                                              num_nodes_out = "numeric",weights = "matrix", bias = "numeric",weight_gradient = "matrix",bias_gradient="numeric"), methods = list(
+                                                initialize = function(num_nodes_in,num_nodes_out,weights,bias,weight_gradient=NA,bias_gradient=NA){
                                                   .self$num_nodes_in <- num_nodes_in
                                                   .self$num_nodes_out <- num_nodes_out
                                                   mat=matrix(weights,nrow=num_nodes_in,ncol=num_nodes_out)
                                                   .self$weights = mat
                                                   .self$bias = bias
+                                                  if (is.na(weight_gradient) && is.na(bias_gradient)){#if weight gradient and bias gradient not given in initialization, set to zero
+                                                    .self$weight_gradient = matrix(0L, nrow = num_nodes_in, ncol = num_nodes_out) 
+                                                    .self$bias_gradient = rep(0,length(bias))
+                                                  }
+                                                  else {
+                                                    .self$weight_gradient = weight_gradient
+                                                    .self$bias_gradient = bias_gradient
+                                                  }
+                                                },
+                                                ActivationFunction = function(output){ #sigmoid activation function, later i will also implement backpropagation analytically (shape of activation fct. will then become important)
+                                                  return (sapply(output,FUN=function(x) 1/(1+exp(-x))))
                                                 },
                                                 CalcOutput = function(input)
                                                 {
@@ -37,8 +28,8 @@ layer <- setRefClass("Layer", fields = list(num_nodes_in = "numeric",
                                                   output =.self$bias
                                                   output = output + input %*% .self$weights
                                                   
-                                                  print(output)
-                                                  return (output)
+                                                  #print(.self$ActivationFunction(output))
+                                                  return (.self$ActivationFunction(output))
                                                 }
                                               ))
 
@@ -49,9 +40,11 @@ NeuralNetwork <- setRefClass("NN",fields = list(layer_sizes = "numeric",num_node
                                             #weights ist hier eine list of lists
                                             #biases ist hier auch eine list of lists
                                             methods = list(
-                                                  intialize = function(layer_sizes,weights,bias){
+                                                  intialize = function(layer_sizes){
                                                    .self$layer_sizes <- layer_sizes
-                  
+                                                   .self$num_nodes_out <- num_nodes_out
+                                                   .self$weights <- weights
+                                                   .self$bias <- bias
                                                   },
                                                   
                                                   GetLayers = function(){ 
@@ -62,11 +55,14 @@ NeuralNetwork <- setRefClass("NN",fields = list(layer_sizes = "numeric",num_node
                                                     return(layers)
                                                   },
                                                   
-                                                  CalcOutputNN = function(inputx,inputy)
+                                                  CalcOutputNN = function(inputx,inputy,layers=NA)
                                                   {
                                                     input = c(inputx,inputy)
-                                                    layers = .self$GetLayers()
-              
+                                                    
+                                                    if (is.na(layers[1])){
+                                                      layers = .self$GetLayers()  #set layers to default if no layers are given as input
+                                                    }
+                                                    
                                                     for (layer in layers){
                                                       input = layer$CalcOutput(input)
                                                       
@@ -79,3 +75,91 @@ NeuralNetwork <- setRefClass("NN",fields = list(layer_sizes = "numeric",num_node
                                                   }
                                                 ))
 
+
+random_weights_biases <- function(layer_sizes,range_weights){
+  min_range = min(range_weights)
+  max_range = max(range_weights)
+  random_weights_mat = runif(n=prod(layer_sizes),min=min_range,max=max_range)
+  random_mat1 = matrix(head(random_weights_mat,(layer_sizes[1]*layer_sizes[2])),nrow=layer_sizes[1],ncol=layer_sizes[2])
+  random_mat2 = matrix(tail(random_weights_mat,(layer_sizes[2]*layer_sizes[3])),nrow=layer_sizes[2],ncol=layer_sizes[3])
+  random_mats = list(random_mat1,random_mat2)
+  
+  random_weights_biases=runif(n=layer_sizes[2]+layer_sizes[3],min=min_range,max=max_range)
+  random_bias_1 = head(random_weights_biases,layer_sizes[2])
+  random_bias_2 = tail(random_weights_biases,layer_sizes[3])
+  random_biases  =list(random_bias_1,random_bias_2)
+  
+  return(c(random_mats,random_biases))
+}
+
+
+Learn <- function(data_train,nn){
+  
+  stepwidth = 0.001
+  output_train = mapply(nn$CalcOutputNN,data_train$x,data_train$y)
+  z_train = apply(output_train,MARGIN=2,nn$Classify)
+  classification_error=base::sum(z_train-data$bin_class)**2/length(data$bin_class) #starting value, to be optimized
+  
+  #get gradients of respective position
+  layers <- nn$GetLayers()
+  layer_depth=1
+  for (layer in layers){
+    weight_gradient <- matrix(nrow=layer$num_nodes_in,ncol=layer$num_nodes_out)
+    bias_gradient <- rep(NA,layer$num_nodes_out)
+    for (i in 1:layer$num_nodes_in){
+      for (j in 1:layer$num_nodes_out){
+        #update weight (increment by stepwidth, approximate differential)
+        layer$weights[i,j]=layer$weights[i,j]+stepwidth
+        layers[[layer_depth]] <- layer
+        #calculate_updated output
+        output_train = mapply(nn$CalcOutputNN,data_train$x,data_train$y,MoreArgs = list(layers))
+        #classifiy updated Output
+        z_train = apply(output_train,MARGIN=2,nn$Classify)
+        #calculate new error
+        classification_error_new=base::sum(z_train-data$bin_class)**2/length(data$bin_class) #starting value, to be optimized
+        #calculate direction of learning and stepwidth
+        delta_cost = classification_error_new - classification_error#das muss automatisch auch die layer in dem nn updaten -> inheritance! sonst muss ich immer neu die nn klasse überschreiben
+        weight_gradient[i,j] <- delta_cost/stepwidth
+        #reset weights
+        layer$weights[i,j]=layer$weights[i,j]-stepwidth #reset to starting position, i just wanted to screen sensitivity
+      
+      }
+    }
+    for (i in 1:layer$num_nodes_out){
+      layer$bias[i]=layer$bias[i]+stepwidth
+      layers[[layer_depth]] <- layer
+      output_train = mapply(nn$CalcOutputNN,data_train$x,data_train$y,MoreArgs = list(layers))
+      z_train = apply(output_train,MARGIN=2,nn$Classify)
+      classification_error_new=base::sum(z_train-data$bin_class)**2/length(data$bin_class) #starting value, to be optimized
+      delta_cost = classification_error_new - classification_error#das muss automatisch auch die layer in dem nn updaten -> inheritance! sonst muss ich immer neu die nn klasse überschreiben
+      bias_gradient[i] <- delta_cost/stepwidth
+      layer$bias[i]=layer$bias[i]-stepwidth #reset to starting position, i just wanted to screen sensitivity
+    }
+    layer$weight_gradient <- weight_gradient
+    layer$bias_gradient <- bias_gradient
+    #perform learning step
+    layer_new<-ApplyGradient(learnrate=0.01,layer)
+    layers[[layer_depth]]<-layer_new
+    layer_depth = layer_depth+1
+  }
+  
+  return(layers) #return updated layers after one learning iteration, note that a termination requirement (error<epsilon) is programmed into the app
+}
+
+ApplyGradient<-function(learnrate,layer){
+  #for one specific layer I update the layer weights according to the gradients discovered in the learn step
+  bias_tmp <- layer$bias
+  weights_tmp <- layer$weights
+  bias_gradient <- layer$bias_gradient
+  weight_gradient <- layer$weight_gradient
+  for (i in 1:layer$num_nodes_out){
+    bias_tmp[i] <- bias_tmp[i] - bias_gradient[i]*learnrate
+    for (j in 1:layer$num_nodes_in){
+      weights_tmp[j,i] <- weights_tmp[j,i] - weight_gradient[j,i]*learnrate
+    }
+  }
+  layer$bias <- bias_tmp
+  print(layer$bias)
+  layer$weights <- weights_tmp
+  return(layer)
+}
--- app.R
+++ app.R
@@ -10,6 +10,8 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(akima)
+library(tidyr)
 
 
 path=getwd()
@@ -27,21 +29,21 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     headerPanel("Decision Boundary Params (One Hidden Layer with 3 Nodes)"),
     sidebarPanel(
-      sliderInput(inputId = "W11", label = "W11", min = -1, max = 1, value = 0.2, step = stepWidth),
+      sliderInput(inputId = "W11", label = "W11", min = -1, max = 1, value = 0.1, step = stepWidth),
       sliderInput(inputId = "W12", label = "W12", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "W13", label = "W13", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "W21", label = "W21", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "W22", label = "W22", min = -1, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "W13", label = "W13", min = -1, max = 1, value = 0.3, step = stepWidth),
+      sliderInput(inputId = "W21", label = "W21", min = -1, max = 1, value = 0.4, step = stepWidth),
+      sliderInput(inputId = "W22", label = "W22", min = -1, max = 1, value = 0.5, step = stepWidth),
       sliderInput(inputId = "W23", label = "W23", min = -1, max = 1, value = 0.6, step = stepWidth),
-      sliderInput(inputId = "bias1_1", label = "b1_1", min = 0, max = 1, value = 0.6, step = stepWidth),
-      sliderInput(inputId = "bias2_1", label = "b2_1", min = 0, max = 1, value = 0.6, step = stepWidth),
-      sliderInput(inputId = "bias3_1", label = "b3_1", min = 0, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "bias1_1", label = "b1_1", min = -1, max = 1, value = 0.7, step = stepWidth),
+      sliderInput(inputId = "bias2_1", label = "b2_1", min = -1, max = 1, value = 0.8, step = stepWidth),
+      sliderInput(inputId = "bias3_1", label = "b3_1", min = -1, max = 1, value = 0.9, step = stepWidth),
       sliderInput(inputId = "A11", label = "A11", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "A21", label = "A21", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "A31", label = "A31", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "A12", label = "A12", min = -1, max = 1, value = 0.2, step = stepWidth),
-      sliderInput(inputId = "A22", label = "A22", min = -1, max = 1, value = 0.6, step = stepWidth),
-      sliderInput(inputId = "A32", label = "A32", min = -1, max = 1, value = 0.6, step = stepWidth),
+      sliderInput(inputId = "A21", label = "A21", min = -1, max = 1, value = 0.4, step = stepWidth),
+      sliderInput(inputId = "A31", label = "A31", min = -1, max = 1, value = 0.5, step = stepWidth),
+      sliderInput(inputId = "A12", label = "A12", min = -1, max = 1, value = 0.1, step = stepWidth),
+      sliderInput(inputId = "A22", label = "A22", min = -1, max = 1, value = 0.7, step = stepWidth),
+      sliderInput(inputId = "A32", label = "A32", min = -1, max = 1, value = 0.8, step = stepWidth),
       sliderInput(inputId = "bias1_2", label = "b1_2", min = -1, max = 1, value = 1, step = stepWidth),
       sliderInput(inputId = "bias2_2", label = "b2_2", min = -1, max = 1, value = 0, step = stepWidth)
     ),
@@ -49,7 +51,8 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("ClassifierPlot")
+           plotOutput("ClassifierPlot"),
+           textOutput("selected_var")
            
         )
     
@@ -60,38 +63,62 @@ server <- function(input, output) {
       
     
     data <- data.frame(x=runif(100),y=runif(100)) %>% 
-      mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) 
+      mutate(label=ifelse(test=sqrt((x**2+y**2))<=0.5,yes = "red" ,no="blue")) %>% mutate(bin_class = ifelse(label=="red",yes=1,no=0))
       
     output$ClassifierPlot <- renderPlot({
         
+        layer_sizes = c(2,3,2)
         # generate bins based on input$bins from ui.R
-        mat_1 = matrix(c(input$W11,input$W12,input$W13,input$W21,input$W22,input$W23),nrow=2,ncol=3)
-        mat_2 = matrix(c(input$A11,input$A21,input$A31,input$A12,input$A22,input$A32),nrow=3,ncol=2)
+        mat_1 = matrix(c(input$W11,input$W12,input$W13,input$W21,input$W22,input$W23),nrow=layer_sizes[1],ncol=layer_sizes[2])
+        mat_2 = matrix(c(input$A11,input$A21,input$A31,input$A12,input$A22,input$A32),nrow=layer_sizes[2],ncol=layer_sizes[3])
         weights_list = list(mat_1,mat_2)
-        bias_1 = c(input$bias1_1,input$bias2_1)
+        bias_1 = c(input$bias1_1,input$bias2_1,input$bias3_1)
         bias_2 = c(input$bias1_2,input$bias2_2)
         biases = list(bias_1,bias_2)
-        layer_sizes = c(2,3,2)
+        #weights_list <- head(random_init,2)
+        #biases <- tail(random_init,2)
         
         nn <- NeuralNetwork$new(layer_sizes=layer_sizes,num_nodes_out=2,weights=weights_list,bias=biases)
+        #randomize initial weights
+        #TBA Later:Update displayed weights (sliders) according to learning process
+        random_init <- random_weights_biases(layer_sizes=layer_sizes,range_weights=c(-1,1))
+        nn$weights <- head(random_init,2)
+        nn$bias <- tail(random_init,2)
+        
+        
         
         x=seq(from=0,to=1,length.out=10)
         y=x
         data_grid=expand.grid(x=x,y=y)
         
-        output=mapply(nn$CalcOutputNN,data_grid$x,data_grid$y) #to visualize decision boundary
-  
-        z=apply(output,MARGIN=2,nn$Classify)
-  
-        data_grid <- data_grid %>% mutate(z=z)
+        #output=mapply(nn$CalcOutputNN,data_grid$x,data_grid$y,MoreArgs=list(layers=nn$GetLayers())) #to visualize decision boundary
+        #z=apply(output,MARGIN=2,nn$Classify) #contour of classification of grid ("pixels of plot")
         
-        v <- ggplot(data_grid, aes(x, y, fill= z)) + 
-          geom_tile(alpha=0.2)
-        v<-v + geom_point(data=data, aes(x=x, y=y,colour=label))
-        print(v)
+        #data_grid <- data_grid %>% mutate(z=z)
         
+
+        output_train = mapply(nn$CalcOutputNN,data$x,data$y)
+        z_train = apply(output_train,MARGIN=2,nn$Classify)
+        classification_error=base::sum(z_train-data$bin_class)**2/length(data$bin_class)
+        print(classification_error)
+        v <- ggplot(data=data, aes(x=x, y=y,colour=label))+
+            geom_point()
         
-    })
+        while(classification_error>0.1){ #loop terminates when error <=0.1
+          layers <- Learn(data,nn)
+          output_train = mapply(nn$CalcOutputNN,data$x,data$y, MoreArgs = list(layers=layers))
+          z_train = apply(output_train,MARGIN=2,nn$Classify)
+          data_grid <- data_grid %>% mutate(z=z_train)
+          classification_error=base::sum(z_train-data$bin_class)**2/length(data$bin_class)
+          v+geom_raster(data=data_grid,aes(x,y,alpha=0.2,fill=z_train),interpolate = TRUE)
+          print(v)
+          print(paste("Average Error per Input:",classification_error))  
+        }
+        
+      
+        #end ClassifierPlot
+        })
+    
 }
 
 # Run the application 
